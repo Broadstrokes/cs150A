bf37f3e35e3f37d06393d124b663510ae362ca54
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-02 21:58:54 -0700

    Complete task 2

--- A04_sajjad.sql
+++ A04_sajjad.sql
@@ -14,6 +14,23 @@ from vt_animals
   )
 
 /*  TASK 02  */
+select 
+z_em_Emp.D_ID as "EM_Emp.D_ID"
+, z_em_Dept.D_ID as "EM_Dept.D_ID"
+, D_Name
+from z_em_Dept 
+LEFT JOIN z_em_Emp ON z_em_Dept.d_id = z_em_Emp.D_ID
+where z_em_Emp.D_ID IS NULL;
+
+select 
+	vt_animals.cl_id,
+	vt_clients.cl_id,
+	vt_clients.cl_name_last
+from vt_clients cl
+left join vt_animals an
+on vt_clients.cl_id = vt_animals.cl_id
+where vt_animals.cl_id is null
+;
 
 
 /*  TASK 03 */
