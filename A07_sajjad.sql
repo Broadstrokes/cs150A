75ec0395bc1c9facb72c8c5ab5477da88c8a5c7a
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-13 23:02:35 -0500

    A07 - Complete task 2

--- A07_sajjad.sql
+++ A07_sajjad.sql
@@ -41,4 +41,52 @@ create table bk_order_details(
 	primary key(order_id, order_line),
 	foreign key(order_id) references bk_order_headers,
 	foreign key(book_id)	references bk_books
-);
+);/*
+TASK 02
+Write a minimum of 3 insert statements for each table using the sample data provided.  You should not write insert statements for ALL of the data, just select data for a minimum of three rows for each table.  Be careful to maintain referential integrity when choosing rows.
+*/
+
+/* bk_customers inserts */
+insert into bk_customers (cust_id, cust_name_last, cust_name_first, cust_state, cust_postal_code, cust_acct_opened)
+  values (226656, 'Randall', 'Randall', 'NJ', 8251, '2012-08-08');
+
+insert into bk_customers (cust_id, cust_name_last, cust_name_first, cust_state, cust_postal_code, cust_acct_opened)
+  values (259906, 'Capybara', 'Wile E.', 'CA', 94132, '2014-08-05');
+
+insert into bk_customers (cust_id, cust_name_last, cust_name_first, cust_state, cust_postal_code, cust_acct_opened)
+  values (221297, 'Dodgson', 'Charles', 'MI', 49327, '2001-05-06');
+
+/* bk_books inserts */
+insert into bk_books (book_id, title, year_publd, isbn, page_count, list_price)
+		values (1401, 'Visual Studio Tools for Office', 2006, '321334884', 976, 54.99);
+
+insert into bk_books (book_id, title, year_publd, isbn, page_count, list_price)
+		values (1305, 'Journeys Through Flatland', 1958, '387515357', 100, 9.99);
+
+insert into bk_books (book_id, title, year_publd, isbn, page_count, list_price)
+		values (1101, 'Programming SQL Server with VB.NET', 2002, '735615357', 300, 59.99);
+
+
+/* bk_order_headers inserts */
+insert into bk_order_headers(order_id, order_date, cust_id)
+	values (12900, '2015-01-25', 226656);
+
+insert into bk_order_headers(order_id, order_date, cust_id)
+	values (12901, '2015-01-26', 259906);
+
+insert into bk_order_headers(order_id, order_date, cust_id)
+	values (12855, '2015-01-18', 221297);
+
+
+/* bk_order_details inserts */
+insert into bk_order_details (order_id, order_line, cust_id, book_id, quantity, order_price)
+	values (12900, 1, 226656, 1401, 20, 50);
+
+insert into bk_order_details (order_id, order_line, cust_id, book_id, quantity, order_price)
+	values (12900, 2, 226656, 1305, 125, 5);
+
+insert into bk_order_details (order_id, order_line, cust_id, book_id, quantity, order_price)
+	values (12901, 1, 259906, 1401, 50, 49);
+
+insert into bk_order_details (order_id, order_line, cust_id, book_id, quantity, order_price)
+	values (12855, 1, 221297, 1101, 5, 59.99);
